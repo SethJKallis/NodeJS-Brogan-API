8141d1bdcac75d143be346210ba1fea5a61d9013
SethJKallis <kallisseth@gmail.com>
2023-02-14 14:46:38 +0200

    first restful api commit /w tim

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "buionaiwv6zmwczcjobx-mysql.services.clever-cloud.com",
-    USER: "u9gxhqzgpmwvq3up",
-    PASSWORD: "Tm22wmfjOYSMynojQLUn",
-    DB: "buionaiwv6zmwczcjobx"
+    HOST: "bqmegd3wmhjeemrjjnwd-mysql.services.clever-cloud.com",
+    USER: "u7bm1z3nzrrbmexr",
+    PASSWORD: "gLPVvRAB3GwcLhoZFeIv",
+    DB: "bqmegd3wmhjeemrjjnwd"
 };
